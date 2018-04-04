ab56ee73eae2f615f7b362f8d64a05c958094867
JoanesMiranda <joanestecnico@gmail.com>
2018-04-03 21:53:46 -0300

    Iniciado o desenvolvimento da pagina de tarefas

---
+++ app/views/tarefas.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css"  href="../../bibliotecas/bootstrap/css/bootstrap.min.css" >
+        <link rel="stylesheet" type="text/css"  href="../../bibliotecas/font-awesome/css/font-awesome.min.css" >
+        <link rel="stylesheet" type="text/css"  href="../../css/sb-admin.min.css" >
+        <title>Lista de Tarefas</title>
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Atividades</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-auto  mt-5">
+                    <a class="btn btn-sm btn-info" href=""><i class="fa fa-fw fa-book"> </i>Atividades do Evento</a>
+                    <a class="btn btn-sm btn-light" href=""><i class="fa fa-fw fa-print"> </i>Imprimir</a>
+                </div>
+            </div>
+            <div class="row ">
+                <div class="col-md-auto  mt-5">
+                    <div class="card">
+                        <div class="card-body">
+                            This is some text within a card body.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../../bibliotecas/jquery/jquery.min.js" ></script>
+    <script src="../../bibliotecas/bootstrap/js/bootstrap.bundle.min.js" ></script>
+    <script src="../../bibliotecas/jquery-easing/jquery.easing.min.js" ></script>
+    <script type="text/javascript" src="../../js/sb-admin.js" ></script>
+</body>
+</html>
